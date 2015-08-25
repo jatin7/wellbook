03175e563e2e1f9026ae08f299dbed6740edc881
Vedant Jain <vedjain85@gmail.com>
2015-08-25 00:11:17 -0500

    Update hive_setup.sh

--- q-d_demo/hive_setup.sh
+++ q-d_demo/hive_setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 message 'Creating wells table'
-hive -f ~/wellbook/ddl/wells.ddl
+hive -f ~/wellbook/q-d_demo/ddl/wells.ddl
 message 'Creating water_sites table'
-hive -f ~/wellbook/ddl/water_sites.ddl
+hive -f ~/wellbook/q-d_demo/ddl/water_sites.ddl
 message 'Creating formations table'
-hive -f ~/wellbook/ddl/formations.ddl
+hive -f ~/wellbook/q-d_demo/ddl/formations.ddl
 message 'Creationg formations_key table'
-hive -f ~/wellbook/ddl/formations_key.ddl
+hive -f ~/wellbook/q-d_demo/ddl/formations_key.ddl
 message 'Creating log_metadata table'
-hive -f ~/wellbook/ddl/log_metadata.ddl
+hive -f ~/wellbook/q-d_demo/ddl/log_metadata.ddl
 message 'Creating log_readings table'
-hive -f ~/wellbook/ddl/log_readings.ddl
+hive -f ~/wellbook/q-d_demo/ddl/log_readings.ddl
 message 'Creating production table'
-hive -f ~/wellbook/ddl/production.ddl
+hive -f ~/wellbook/q-d_demo/ddl/production.ddl
 message 'Creating injections table'
-hive -f ~/wellbook/ddl/injections.ddl
+hive -f ~/wellbook/q-d_demo/ddl/injections.ddl
 message 'Creationg scout_tickets table'
-hive -f ~/wellbook/ddl/scout_tickets.ddl
+hive -f ~/wellbook/q-d_demo/ddl/scout_tickets.ddl
 message 'Creating auctions table'
-hive -f ~/wellbook/ddl/auctions.ddl
+hive -f ~/wellbook/q-d_demo/ddl/auctions.ddl
 message 'Creating well_surveys table'
-hive -f ~/wellbook/ddl/well_surveys.ddl
+hive -f ~/wellbook/q-d_demo/ddl/well_surveys.ddl
